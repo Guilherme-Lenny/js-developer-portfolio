ccac21eb8d65a5e972cb1cb522ca3189ebf422be
Guilherme Lenny <guilherme_kenay@outlook.com>
2024-06-30 20:54:32 -0300

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Guilherme-Lenny/js-developer-portfolio/main/assets/js/main.js';
+    const url = 'https://raw.githubusercontent.com/Guilherme-Lenny/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
